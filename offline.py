cf71696e5278f30fffb15cdf1c2bc587d0a2b6bc
fruchterg <57596198+fruchterg@users.noreply.github.com>
2020-08-28 00:09:22 +0300

    offline-function

--- offline.py
+++ offline.py
@@ -1,13 +1,12 @@
 import re
 import os
+import shelve
 
-dict_data = {}
 dictFile = {}
 listString = []
 
 
 def ignorecharacter(string):
-
     return re.sub('[^a-z0-9' ']+', '', string.lower())
 
 
@@ -19,7 +18,6 @@ def read_from_file():
 
 
 def init_data(file, indexsource):
-
     with open(file, encoding='UTF-8') as the_file:
         temp_list_string = the_file.read().split("\n")
     for i, str in enumerate(temp_list_string, 1):
@@ -27,25 +25,27 @@ def init_data(file, indexsource):
 
 
 def init_sub():
-
-     for j in range(len(listString)):
-         for i in range(len(listString[j]["sentence"]) + 1):
-             for k in range(i+1, len(listString[j]["sentence"])+1):
-                dict_data.setdefault(ignorecharacter(listString[j]["sentence"][i:k]), set()).add(j)
-                dict_data.setdefault(ignorecharacter(listString[j]["sentence"][k:i]), set()).add(j)
+    db_file = shelve.open('sucsses.db', writeback=True)
+    for j in range(len(listString)):
+        for i in range(len(listString[j]["sentence"]) + 1):
+            for k in range(i + 1, len(listString[j]["sentence"]) + 1):
+                try:
+                    db_file[ignorecharacter(listString[j]["sentence"][i:k])].add(j)
+                    db_file[ignorecharacter(listString[j]["sentence"][k:i])].add(j)
+                except:
+                    db_file[ignorecharacter(listString[j]["sentence"][i:k])] = set()
+                    db_file[ignorecharacter(listString[j]["sentence"][k:i])] = set()
+    db_file.close()
 
 
 def sort_best_complete():
-    for sentence in dict_data.keys():
-        dict_data[sentence] = list(dict_data[sentence])
-        dict_data[sentence] = sorted(dict_data[sentence],
-            key=lambda k: listString[k]["sentence"])
+    db_file = shelve.open('sucsses.db', writeback=True)
+    for sentence in db_file.keys():
+        db_file[sentence] = list(db_file[sentence])
+        db_file[sentence] = sorted(db_file[sentence], key=lambda k: listString[k]["sentence"])
+    db_file.close()
 
 
 read_from_file()
 init_sub()
 sort_best_complete()
-
-
-
-
